616819d29045497eb4c1a72c7125dff6b00191ec
muhamedflawless <muhamed.hamza@elev.ga.ntig.se>
2024-02-01 15:49:43 +0100

    15:49

--- app.rb
+++ app.rb
@@ -17,3 +17,31 @@ end
 get('/signup') do
   slim(:signup)
 end
+
+get('/accounts') do
+  slim(:accounts)
+end
+
+get('/transactions') do
+  slim(:transactions)
+end
+
+get('/onlineServices') do
+  slim(:onlineServices)
+end
+
+get('/security') do
+  slim(:security)
+end
+
+get('/contactUs') do
+  slim(:contactUs)
+end
+
+get('/aboutUs') do
+  slim(:aboutUs)
+end
+
+get('/termsAndConditions') do
+  slim(:termsAndConditions)
+end
